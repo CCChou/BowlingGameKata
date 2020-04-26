79194bdfbafd662cbbd1f9547581a041d1a115ca
dennischou <dses5900@gmail.com>
2020-04-26 15:36:40 +0800

    Modify the first test
    Add score method and make the test pass.

--- src/test/java/dennis/kata/bowlinggame/BowlingGameTest.java
+++ src/test/java/dennis/kata/bowlinggame/BowlingGameTest.java
@@ -2,10 +2,15 @@ package dennis.kata.bowlinggame;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameTest {
     @Test
     public void testGame() {
         Game game = new Game();
-        game.roll(0);
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
     }
 }
